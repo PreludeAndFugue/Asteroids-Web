9ef8e06995094b17d21be06ecd45c543c5e9fc2f
Gary Kerr <gdrummondk@gmail.com>
2011-07-06 01:07:26 +0100

    Updated build script.

--- builder.py
+++ builder.py
@@ -1,13 +1,17 @@
-
+"""
+A build script for copying source files to bin dir.
+"""
 import os
 from os.path import join
 import shutil
 
 def main():
     build = r'..\bin'
-    files = r'asteroids.css  asteroids.html asteroids.js asteroids.txt'.split()
+    files = 'asteroids.css  asteroids.html asteroids.js asteroids.txt'.split()
     srcdir = os.getcwd()
     builddir = os.path.normpath(join(srcdir, build))
+    
+    # copy files (will overwrite existing files)
     for f in files:
         shutil.copy(join(srcdir, f), builddir)
         
